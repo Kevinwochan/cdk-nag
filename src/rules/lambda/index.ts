55144a9282f434324f25bcbb959111181c5e56ad
kevinwochan <kevinwochan@gmail.com>
2024-08-29 21:13:49 +1000

    feat(serverless): remove lambda logging rule

--- src/rules/lambda/index.ts
+++ src/rules/lambda/index.ts
@@ -4,9 +4,12 @@ SPDX-License-Identifier: Apache-2.0
 */
 
 export { default as LambdaConcurrency } from './LambdaConcurrency';
+export { default as LambdaDefaultMemorySize } from './LambdaDefaultMemorySize';
+export { default as LambdaDefaultTimeout } from './LambdaDefaultTimeout';
 export { default as LambdaDLQ } from './LambdaDLQ';
 export { default as LambdaFunctionPublicAccessProhibited } from './LambdaFunctionPublicAccessProhibited';
 export { default as LambdaFunctionUrlAuth } from './LambdaFunctionUrlAuth';
 export { default as LambdaInsideVPC } from './LambdaInsideVPC';
 export { default as LambdaLatestVersion } from './LambdaLatestVersion';
-export { default as LambdaLogging } from './LambdaLogging'
+export { default as LambdaTracing } from './LambdaTracing';
+
