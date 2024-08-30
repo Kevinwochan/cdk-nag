c22ee0590e512a1bf557586db57308bf2c3d5330
kevinwochan <kevinwochan@gmail.com>
2024-08-30 14:45:57 +1000

    feat(serverless): removing redundant implementation

--- src/rules/sqs/SQSRedrivePolicy.ts
+++ src/rules/sqs/SQSRedrivePolicy.ts
@@ -1,5 +1,5 @@
 import { CfnResource, Stack } from 'aws-cdk-lib';
-import { CfnQueue, Queue } from 'aws-cdk-lib/aws-sqs';
+import { CfnQueue } from 'aws-cdk-lib/aws-sqs';
 import { NagRuleCompliance } from '../../nag-rules';
 
 /**
@@ -12,14 +12,6 @@ export default function SQSRedrivePolicy(node: CfnResource): NagRuleCompliance {
     const redrivePolicy = Stack.of(node).resolve(node.redrivePolicy);
     if (redrivePolicy !== undefined) return NagRuleCompliance.COMPLIANT;
     return NagRuleCompliance.NON_COMPLIANT;
-  } else if (node instanceof Queue) {
-    // For L2 constructs, we need to check the underlying CfnQueue
-    console.log(node);
-    const dlqConfig = Stack.of(node).resolve(node.deadLetterQueue);
-    console.log(dlqConfig);
-    if (dlqConfig?.maxReceiveCount) return NagRuleCompliance.COMPLIANT;
-    return NagRuleCompliance.NON_COMPLIANT;
   }
-
   return NagRuleCompliance.NOT_APPLICABLE;
 }
